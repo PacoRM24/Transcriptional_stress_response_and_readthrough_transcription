0afc8758b336fc5a15c666ea5276b789e02e4821
Francisco Miguel Rios Martinez <84066882+PacoRM24@users.noreply.github.com>
2023-09-26 13:53:38 -0600

    Update DoGs_and_Genes_upregulated.py

--- DoGs_and_Genes_upregulated.py
+++ DoGs_and_Genes_upregulated.py
@@ -1,21 +1,32 @@
-# This code will extract from the DAVID's gene ID conversion tool the Gene Symbols
-# and compare them with the genes that are upregulated
+# This code will extract Gene Symbols from the DAVID gene ID conversion tool
+# and compare them with the upregulated genes.
 
-conversion_table = r"C:\Users\Paco\Desktop\Pan_ARTDeco_ensGene\analisis_casero_de_expresion_diferencial\ARTDeco_Pan_cells_THZ1_names.txt"
-genes_up_file = r"C:\Users\Paco\Desktop\MCF10A_ARTDeco_ensGene\analisis_casero_de_expresion_diferencial\Osmotic_name_genes.txt"
+# Define the paths to the conversion table and the file containing upregulated genes.
+conversion_table = # Write the path to the conversion table given by DAVIDs conversion tool
+genes_up_file = # Write the path to the list of genes upregulated
 
+# Open the conversion table and upregulated genes file for reading.
 with open(conversion_table, "r") as dogs, open(genes_up_file, "r") as genes_up:
 
+    # Initialize an empty list to store Gene Symbols from the conversion table.
     dogs_names = []
+
+    # Skip the first line in the conversion table.
     dogs.readline()
+
+    # Extract Gene Symbols from the conversion table and store them in dogs_names list.
     for dog in dogs:
         dog_name = dog.strip().split("\t")[1]
         dogs_names.append(dog_name)
 
+    # Initialize a counter variable to keep track of matching genes.
     c = 0
+
+    # Compare genes from the upregulated genes file with those in dogs_names.
     for gene in genes_up:
         gene = gene.strip()
         if gene in dogs_names:
             c += 1
 
+    # Print the count of matching genes.
     print(c)

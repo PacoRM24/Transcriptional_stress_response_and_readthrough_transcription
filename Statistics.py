4571fe5c65ed2d9f55a7b677b78d0a98dcff8fdc
Francisco Miguel Rios Martinez <84066882+PacoRM24@users.noreply.github.com>
2023-09-26 16:13:39 -0600

    Update Statistics.py

--- Statistics.py
+++ Statistics.py
@@ -4,13 +4,12 @@ import numpy as np
 import pandas as pd
 from scipy import stats
 
-# Load the data
+# Load the data from a file with the RPKM values
 expression_data = pd.read_csv(r"C:\Users\Paco\Desktop\Pan_ARTDeco_ensGene\analisis_casero_de_expresion_diferencial\MAtriz_de_expresion_THZ1.txt", sep="\t")
 
 # Logarithmic transformation of the data
 control_columns = ['DMSOR1', 'DMSOR2']
-tpl_columns = ['THZ1R1', 'THZ1R2']
-
+tpl_columns = ['THZ1R1', 'THZ1R2'] #Write the name columns depending on the data analyzed, TPL or THZ1
 expression_data[control_columns] = np.log1p(expression_data[control_columns])
 expression_data[tpl_columns] = np.log1p(expression_data[tpl_columns])
 
@@ -34,14 +33,14 @@ for i in range(len(expression_data)):
     t_statistics.append(t_statistic)
 
 # Calculate de log2 fold change
-conditions = ['DMSO', 'THZ1']
+conditions = ['DMSO', 'THZ1'] # Change the problem condition according to the file used
 data_combined = pd.DataFrame()
 
 for condition in conditions:
     condition_columns = [col for col in expression_data.columns if condition in col]
     data_combined[condition] = expression_data[condition_columns].mean(axis=1)
 
-data_fold_change = np.log2((data_combined["THZ1"] + sc) / (data_combined["DMSO"] + sc))
+data_fold_change = np.log2((data_combined["THZ1"] + sc) / (data_combined["DMSO"] + sc)) # Change THZ1 if it is the case
 
 # Add the log2fc, p-values and t statistics to the data frame
 expression_data['log2fc'] = data_fold_change
@@ -49,4 +48,4 @@ expression_data['t_statistic'] = t_statistics
 expression_data['p-value'] = p_values
 
 # Generate a new file with all the data
-expression_data.to_csv("ARTDeco_Pan_cells_Tabla_de_expresion_de_THZ1.csv", index= False)
+expression_data.to_csv("ARTDeco_Pan_cells_Tabla_de_expresion_de_THZ1.csv", index= False) #Change the name if necessary

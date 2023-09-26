a7b4b66f8a95cd7611a9c47eca7de4c61d73863b
Francisco Miguel Rios Martinez <84066882+PacoRM24@users.noreply.github.com>
2023-09-26 13:44:17 -0600

    Update Common_DoGs.py

--- Common_DoGs.py
+++ Common_DoGs.py
@@ -4,13 +4,19 @@ dog_file1 = r"C:\Users\Paco\Desktop\Pan_ARTDeco_ensGene\analisis_casero_de_expre
 dog_file2 = r"C:\Users\Paco\Desktop\Pan_ARTDeco_ensGene\analisis_casero_de_expresion_diferencial\ARTDeco_Pan_cells_THZ1_unic_dogs.bed"
 common_dogs = r"C:\Users\Paco\Desktop\Pan_ARTDeco_ensGene\analisis_casero_de_expresion_diferencial\ARTDeco_Pan_cells_TPLyTHZ1_unic_common_dogs.bed"
 
+# Open the files and iterate through them
 with open(dog_file1, "r") as file1, open(dog_file2, "r") as file2, open(common_dogs, "w") as common:
+    
+    # Save a DoGs file in a list
     dog2_data = [line.strip() for line in file2]
+
+    # Save the IDs from one DoGs file
     for dog1 in file1:
         dog1_id = dog1.strip().split("\t")[3]
 
+        # Iterate through the second file to find the IDs that match between files
         for dog2 in dog2_data:
             dog2_id = dog2.strip().split("\t")[3]
 
             if dog1_id == dog2_id:
-                common.write(dog1)
+                common.write(dog1) # Write the DoG that match in the two files

e11a46f560427f4bb6b9cf10ac17eb3728997f53
Francisco Miguel Rios Martinez <84066882+PacoRM24@users.noreply.github.com>
2023-09-26 16:25:25 -0600

    Update Heatmap_log2fc.py

--- Heatmap_log2fc.py
+++ Heatmap_log2fc.py
@@ -1,45 +1,48 @@
+# This code will generate a heatmap after calculate the log2 fold change 
+
+# Import necessary libraries
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Cargar el archivo CSV en un DataFrame y especificar que la primera columna es el índice
+# Load the CSV file into a DataFrame, specifying the first column as the index
 data = pd.read_csv(r"C:\Users\Paco\Desktop\MCF10A_ARTDeco_ensGene\analisis_casero_de_expresion_diferencial\Matriz_de_expresion_af2.txt", sep="\t", index_col=0)
 
-# Extraer los nombres de las condiciones ('DMSOR' y 'TPLR')
+# Extract the names of the conditions ('DMSO' and 'TPL') or ('DMSO' and 'THZ1')
 conditions = ['DMSO', 'TPL']
 
-# Crear un DataFrame para almacenar los promedios de cada condición
+# Create a DataFrame to store the averages for each condition
 data_combined = pd.DataFrame()
 
-# Calcular el promedio de las réplicas para cada condición
+# Calculate the average for replicates of each condition
 for condition in conditions:
     condition_columns = [col for col in data.columns if condition in col]
     data_combined[condition] = data[condition_columns].mean(axis=1)
 
-# Calcular el log2 del fold change y agregarlo como una columna tomando en cuenta un valor para ajustar los valores iguales a cero
+# Calculate the log2 fold change and add it as a column while considering an adjustment value for values equal to zero
 adj_value = 0.001
 data_fold_change = np.log2((data_combined["TPL"] + adj_value) / (data_combined["DMSO"] + adj_value))
 
-# Crear un DataFrame con los valores de log2 fold change para TPL y THZ1
-#heatmap_data = data_combined[['TPLvsDMSO', 'THZ1vsDMSO']]
+# Create a DataFrame with log2 fold change values for 'TPL' and 'DMSO'
 val_max = max(data_fold_change)
 val_min = min(data_fold_change)
-print(val_max,val_min)
+print(val_max, val_min)
 
-# Crear el heatmap con los datos
+# Create the heatmap with the data
 sns.set(font_scale=1)
 plt.figure(figsize=(10, 6))
-heatmap = sns.heatmap(data_fold_change.to_frame(), cmap="coolwarm", cbar=True, xticklabels=False, yticklabels=False, vmax=9, vmin=-9)  # Establecer yticklabels=False para quitar las etiquetas del eje y
+heatmap = sns.heatmap(data_fold_change.to_frame(), cmap="coolwarm", cbar=True, xticklabels=False, yticklabels=False, vmax=9, vmin=-9)  # Set yticklabels=False to remove y-axis labels
 
-# Agregar una leyenda personalizada
+# Add a custom legend
 cbar = heatmap.collections[0].colorbar
 cbar.set_label("Log2 Fold Change", rotation=270, labelpad=15)
 
-# Agregar el número de genes en la parte inferior
+# Add the number of genes at the bottom
 num_genes = len(data_combined)
 plt.text(0.5, -0.1, f'n={num_genes}', horizontalalignment='center', verticalalignment='center', transform=plt.gca().transAxes)
 
+# Add labels to the axis and show the plot
 plt.ylabel("DoGs")
 plt.xlabel("TPLvsDMSO")
 plt.show()

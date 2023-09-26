04df13ea46920a70aec4db8d262c75e005fbf07d
Francisco Miguel Rios Martinez <84066882+PacoRM24@users.noreply.github.com>
2023-09-26 13:21:37 -0600

    Update Boxplot.py

--- Boxplot.py
+++ Boxplot.py
@@ -4,8 +4,10 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-dogs_file = r"C:\Users\Paco\Desktop\Pan_ARTDeco_ensGene\analisis_casero_de_expresion_diferencial\RPKM_THZ1yDMSO.txt"
+# Save the file that has the length of the DoGs at a variable
+dogs_file = #Put here the path from the file
 
+# Open the file and iterate through it to extract the lengths from the file
 with open(dogs_file, "r") as dogs:
     dogs_length = []
     dogs.readline()
@@ -13,33 +15,34 @@ with open(dogs_file, "r") as dogs:
         dlength = (int(dog.strip().split("\t")[1]))/1000
         dogs_length.append(dlength)
 
+# Count how may DoGs have a length between 3999 and 5000
 c = 0
 for i in dogs_length:
     if i >= 3.999 and i <= 5.000:
         c += 1
 print(c)
+
+# Imprimir los valores máximo, mínimo, mediana y el promedio
 maximo = np.max(dogs_length)
 minimo = np.min(dogs_length)
 mediana = np.median(dogs_length)
 promedio = np.mean(dogs_length)
 print(maximo,minimo,mediana,promedio)
 
-# Crear un boxplot con Seaborn
-sns.set(style="whitegrid")  # Establece el estilo de la cuadrícula
-plt.figure(figsize=(8, 6))  # Tamaño del gráfico
-
-# Utiliza la función boxplot de Seaborn
+# Use seaborn to create the box-plot
+sns.set(style="whitegrid")  # Graph style
+plt.figure(figsize=(8, 6))  # Graph size
 sns.boxplot(y=dogs_length)
 
-# Personaliza el eje y
-plt.ylim(0, 90)  # Establece los límites en el eje y (ajusta según tus datos)
-plt.yticks(np.arange(0, 91, 10))  # Establece los valores de los ticks en el eje y (ajusta según tus datos)
+# Personalize y axix
+plt.ylim(0, 90)  # Establish the y axis limits
+plt.yticks(np.arange(0, 91, 10))  # Establish the values for y axis ticks
 
-# Personaliza el gráfico (opcional)
-plt.title('DoGs length in pancreatic cancer cells with THZ1')
-plt.ylabel('Length (KB)')
-plt.xlabel(f'n={len(dogs_length)}')
-plt.grid(axis='y')  # Agrega una cuadrícula en el eje y
+# Personalize the graph
+plt.title('DoGs length in pancreatic cancer cells with THZ1') # Add a tittle
+plt.ylabel('Length (KB)') # Add y axis label
+plt.xlabel(f'n={len(dogs_length)}') #Add x axis label
+plt.grid(axis='y')  
 
-# Mostrar el gráfico
+# Show the graph
 plt.show()

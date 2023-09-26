c991970f3d1c8450423e6595681b4e918c04cf3a
Francisco Miguel Rios Martinez <84066882+PacoRM24@users.noreply.github.com>
2023-09-26 13:47:06 -0600

    Update Common_DoGs.py

--- Common_DoGs.py
+++ Common_DoGs.py
@@ -1,8 +1,9 @@
 # This code will compare the IDs from two bed files and write the DoGs in common
 
-dog_file1 = r"C:\Users\Paco\Desktop\Pan_ARTDeco_ensGene\analisis_casero_de_expresion_diferencial\ARTDeco_Pan_cells_TPL_unic_dogs.bed"
-dog_file2 = r"C:\Users\Paco\Desktop\Pan_ARTDeco_ensGene\analisis_casero_de_expresion_diferencial\ARTDeco_Pan_cells_THZ1_unic_dogs.bed"
-common_dogs = r"C:\Users\Paco\Desktop\Pan_ARTDeco_ensGene\analisis_casero_de_expresion_diferencial\ARTDeco_Pan_cells_TPLyTHZ1_unic_common_dogs.bed"
+# Save the files in variables
+dog_file1 = # Write PATH to file1
+dog_file2 = # Write PATH to file2
+common_dogs = # Write the PATH where the output file should go
 
 # Open the files and iterate through them
 with open(dog_file1, "r") as file1, open(dog_file2, "r") as file2, open(common_dogs, "w") as common:

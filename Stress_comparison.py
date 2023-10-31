a969acbf2261d5a99b9e7b72e4a38b96104123ff
Francisco Miguel Rios Martinez <84066882+PacoRM24@users.noreply.github.com>
2023-10-30 23:43:33 -0600

    Update Stress_comparison.py

--- Stress_comparison.py
+++ Stress_comparison.py
@@ -5,24 +5,24 @@
 conversion_table = # Write the path to the conversion table given by DAVIDs conversion tool
 osmotic_file = # Write the path to the list of DoG-producing genes from osmotic stress
 
-# Open the conversion table and upregulated genes file for reading.
+# Open the conversion table and osmotic genes file for reading
 with open(conversion_table, "r") as dogs, open(osmotic_file, "r") as osmotic:
 
-    # Initialize an empty list to store Gene Symbols from the conversion table.
+    # Initialize an empty list to store Gene Symbols from the conversion table
     dogs_names = []
 
-    # Skip the first line in the conversion table.
+    # Skip the first line in the conversion table
     dogs.readline()
 
-    # Extract Gene Symbols from the conversion table and store them in dogs_names list.
+    # Extract Gene Symbols from the conversion table and store them in dogs_names list
     for dog in dogs:
         dog_name = dog.strip().split("\t")[1]
         dogs_names.append(dog_name)
 
-    # Initialize a counter variable to keep track of matching genes.
+    # Initialize a counter variable to keep track of matching genes
     c = 0
 
-    # Compare genes from the upregulated genes file with those in dogs_names.
+    # Compare genes from the osmotic genes file with those in dogs_names
     for gene in osmotic:
         gene = gene.strip()
         if gene in dogs_names:
